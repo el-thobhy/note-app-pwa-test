e0d6a74a25d8cf12b9b032101b1bdb24e3c2393a
yasper <auriwanyasper007@gmail.com>
2025-09-02 21:48:50 +0700

    add pwa settings

---
+++ NoteApp/wwwroot/sw.js
@@ -0,0 +1,15 @@
+﻿const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = ["/", "/css/site.css", "/js/site.js"];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
